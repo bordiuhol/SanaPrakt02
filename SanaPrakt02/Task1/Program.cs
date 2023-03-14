ff4af2d09d973dcd58570e187007f51f768bf157
Bordiuh Olha <kb211_boo@student.ztu.edu.ua>
2023-03-14 03:11:08 +0200

    first 3 messsages changed

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Повідомлення 1");
+Console.WriteLine("Повідомлення 2");
+Console.WriteLine("Повідомлення 3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
